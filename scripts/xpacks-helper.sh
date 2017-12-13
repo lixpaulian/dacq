1281d3111d727d450e752332375b17a2e960040f
Lix N. Paulian <lix@paulian.net>
2017-12-13 21:29:41 +0100

    Updated the script to reflect the new xpack name.

--- scripts/xpacks-helper.sh
+++ scripts/xpacks-helper.sh
@@ -6,14 +6,11 @@
 # Bash helper script used in project generate.sh scripts.
 # -----------------------------------------------------------------------------
 
-do_add_sdi_12_dr_xpack() {
-  local pack_name='sdi-12-dr'
+do_add_dacq_xpack() {
+  local pack_name='dacq'
   do_tell_xpack "${pack_name}-xpack"
 
   do_select_pack_folder "lix/${pack_name}.git"
-
-  do_prepare_dest "${pack_name}/include"
-  do_add_content "${pack_folder}/include"/* 
 
   do_prepare_dest "${pack_name}/src"
   do_add_content "${pack_folder}/src"/* 
